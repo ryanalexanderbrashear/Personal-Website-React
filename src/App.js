686b6e95695b80612313e8f9e57e4ad77f58d55d
Alex Brashear <alexbrashear@gmail.com>
2021-01-07 15:06:46 -0500

    Created basic header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button>Test</Button>
+      <Header />
     </div>
   );
 }
